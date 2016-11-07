ee608ee97ae37ab5ef18981cc310d55567d2a62d
lvdezhong <lvdezhong@gmail.com>
2016-11-07 19:02:04 +0800

    fixbug

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ var outputDir = path.resolve(__dirname, 'dist');
 var plugins = [
     new HtmlWebpackPlugin({
         title: 'minibar',
-        template: './src/index.html'
+        template: './src/index.html',
+        inject: true
     }),
     new webpack.ProvidePlugin({
         _ : 'lodash'
